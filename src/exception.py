a38de43c32748edf01d1733e250bbadd9ad1766c
Ajay Sulya <ajaysulya06@gmail.com>
2025-08-10 04:19:20 +0530

    EDA & Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 def error_message_detail(error,error_detail:sys):
     """
     This function takes an error and its details, formats them into a string,
